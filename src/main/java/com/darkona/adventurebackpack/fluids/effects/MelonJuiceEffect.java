5d784c562906ff89a26dc1e25f9e0f2717b2145c
runescapejon <jonsimpletv@gmail.com>
2016-06-23 18:19:42 -0400

    Uploaded the wrong file on the last commit this fixed issue #13

--- src/main/java/com/darkona/adventurebackpack/fluids/effects/MelonJuiceEffect.java
+++ src/main/java/com/darkona/adventurebackpack/fluids/effects/MelonJuiceEffect.java
@@ -8,35 +8,34 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.world.World;
+import com.darkona.adventurebackpack.util.Utils;
 
 /**
  * Created by Darkona on 12/10/2014.
  */
-public class MelonJuiceEffect extends FluidEffect
-{
+public class MelonJuiceEffect extends FluidEffect {
 
-    public MelonJuiceEffect()
-    {
-        super(ModFluids.melonJuice, 30);
-    }
+	public MelonJuiceEffect() {
+		super(ModFluids.melonJuice, 30);
+	}
 
-    /**
-     * This method determines what will happen to the player when drinking the
-     * corresponding fluid. For example set potion effects, set player on fire,
-     * heal, fill hunger, etc. You can use the world parameter to make
-     * conditions based on where the player is.
-     *
-     * @param world  The World.
-     * @param player The Player.
-     */
-    @Override
-    public void affectDrinker(World world, Entity entity)
-    {
-        if (entity instanceof EntityPlayer)
-        {
-            EntityPlayer player = (EntityPlayer) entity;
-            player.addPotionEffect(new PotionEffect(Potion.digSpeed.getId(), timeInTicks, 0));
-            FluidEffectRegistry.WATER_EFFECT.affectDrinker(world, player);
-        }
-    }
+	/**
+	 * This method determines what will happen to the player when drinking the
+	 * corresponding fluid. For example set potion effects, set player on fire,
+	 * heal, fill hunger, etc. You can use the world parameter to make
+	 * conditions based on where the player is.
+	 *
+	 * @param world
+	 *            The World.
+	 * @param player
+	 *            The Player.
+	 */
+	@Override
+	public void affectDrinker(World world, Entity entity) {
+		if (entity instanceof EntityPlayer) {
+			EntityPlayer player = (EntityPlayer) entity;
+			player.addPotionEffect(new PotionEffect(Potion.digSpeed.getId(), timeInTicks, 0));
+			player.addPotionEffect(new PotionEffect(Potion.regeneration.id, Utils.secondsToTicks(timeInSeconds), 0));
+		}
+	}
 }
