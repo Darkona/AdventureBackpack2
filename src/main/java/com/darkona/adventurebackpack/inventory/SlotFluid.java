9c17ddb0318feab0784be17281edd997d2a88f72
runescapejon <jonsimpletv@gmail.com>
2016-10-08 19:57:19 -0400

    Fixed Issues #21 and https://github.com/GTNewHorizons/NewHorizons/issues/729

--- src/main/java/com/darkona/adventurebackpack/inventory/SlotFluid.java
+++ src/main/java/com/darkona/adventurebackpack/inventory/SlotFluid.java
@@ -1,52 +1,86 @@
 package com.darkona.adventurebackpack.inventory;
 
+import com.darkona.adventurebackpack.item.ItemAdventureBackpack;
+import com.darkona.adventurebackpack.item.ItemHose;
 import com.darkona.adventurebackpack.util.Utils;
 import net.minecraft.inventory.IInventory;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemFishingRod;
+import net.minecraft.item.ItemFlintAndSteel;
+import net.minecraft.item.ItemHoe;
+import net.minecraft.item.ItemShears;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemTool;
 import net.minecraftforge.fluids.FluidContainerRegistry;
 
 /**
  * Created on 12/10/2014
+ * 
  * @author Darkona
  * 
  *
  */
-public class SlotFluid extends SlotAdventureBackpack
-{
+public class SlotFluid extends SlotAdventureBackpack {
 
-    public SlotFluid(IInventory inventory, int id, int x, int y)
-    {
-        super(inventory, id, x, y);
-    }
+	public SlotFluid(IInventory inventory, int id, int x, int y) {
+		super(inventory, id, x, y);
+	}
 
-    public static boolean valid(ItemStack stack)
-    {
-        return ((stack != null) && (FluidContainerRegistry.isContainer(stack)));
-    }
+	public static boolean valid(ItemStack stack) {
+		return ((stack != null) && (FluidContainerRegistry.isContainer(stack)));
+	}
 
-    @Override
-    public boolean isItemValid(ItemStack stack)
-    {
-        return ((stack != null) && (FluidContainerRegistry.isContainer(stack)));
-    }
+	@Override
+	public boolean isItemValid(ItemStack stack) {
+		return ((stack != null) && (FluidContainerRegistry.isContainer(stack)) && isValidTool(stack));
+	}
 
-    @Override
-    public void onSlotChanged()
-    {
+	public static boolean isValidTool(ItemStack stack) {
 
-        if (Utils.inServer())
+		boolean valid = false;
+
+		String[] validToolNames = { "bucket" };
+
+		String[] invalidToolNames = { "cell" };
+
+        if (stack != null && stack.getMaxStackSize() == 1)
         {
-            if (inventory instanceof IInventoryTanks)
-            {
-                ((IInventoryTanks) this.inventory).updateTankSlots();
-            }
-        }
-        super.onSlotChanged();
-    }
+            Item item = stack.getItem();
+            String name = item.getUnlocalizedName().toLowerCase();
 
-    @Override
-    public void putStack(ItemStack par1ItemStack)
-    {
-        super.putStack(par1ItemStack);
-    }
+            for (String toolName : validToolNames)
+            {
+                @SuppressWarnings("unused")
+				String a = toolName;
+                if (name.contains(toolName)) return true;
+            }
+
+            for (String toolName : invalidToolNames)
+            {
+                @SuppressWarnings("unused")
+				String a = toolName;
+                if (name.contains(toolName)) return false;
+            }
+
+        }
+
+	return valid;
+
+}
+
+	@Override
+	public void onSlotChanged() {
+
+		if (Utils.inServer()) {
+			if (inventory instanceof IInventoryTanks) {
+				((IInventoryTanks) this.inventory).updateTankSlots();
+			}
+		}
+		super.onSlotChanged();
+	}
+
+	@Override
+	public void putStack(ItemStack par1ItemStack) {
+		super.putStack(par1ItemStack);
+	}
 }
