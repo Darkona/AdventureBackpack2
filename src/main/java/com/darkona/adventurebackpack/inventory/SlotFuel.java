ff0453ff74d05d2912bc409f8fafb983a7b6a134
Ugachaga <Ugachaga@users.noreply.github.com>
2016-11-17 11:35:30 +0700

    format

--- src/main/java/com/darkona/adventurebackpack/inventory/SlotFuel.java
+++ src/main/java/com/darkona/adventurebackpack/inventory/SlotFuel.java
@@ -9,13 +9,16 @@ import net.minecraft.tileentity.TileEntityFurnace;
  *
  * @author Darkona
  */
-public class SlotFuel extends SlotAdventureBackpack {
-    public SlotFuel(IInventory inventory, int id, int x, int y) {
-		super(inventory, id, x, y);
+public class SlotFuel extends SlotAdventureBackpack 
+{
+    public SlotFuel(IInventory inventory, int id, int x, int y) 
+    {
+	super(inventory, id, x, y);
     }
 
     @Override
-	public boolean isItemValid(ItemStack stack) {
-		return (stack != null) && TileEntityFurnace.isItemFuel(stack) && !SlotFluid.valid(stack); //fuel slot accept only solid fuel
+    public boolean isItemValid(ItemStack stack) 
+    {
+	return (stack != null) && TileEntityFurnace.isItemFuel(stack) && !SlotFluid.valid(stack); //fuel slot accept only solid fuel
     }
 }
